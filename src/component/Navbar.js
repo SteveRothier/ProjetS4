3c873fe75026da9cc66e5828818d675eae6449d5
Steve Rothier <steve.rothier@etudiant.univ-reims.fr>
2022-03-16 15:11:19 +0100

    Changement de la navbar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
     return (
         <nav>
             <ul className="menu">
-                <li><a href="/">Home</a></li>
+                <li><a href="/">Acceuil</a></li>
                 <li><a href="/ressource">Ressources</a></li>
                 <li><a href="/sae">SAE</a></li>
                 <li><a href="/ue">UE</a></li>
